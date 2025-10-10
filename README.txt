d0be5c1d89460cb755244df78e9ea1dcb6a71e32
Jonathan Wang <jwang612@jh.edu>
2025-10-10 11:09:37 -0400

    want to see if autograder accepts this basic submission

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
 TODO: names of team members and their contributions to the project
+Jonathan Wang
+  -made sure code compiles and makefile works as expected
 
 TODO (for MS3): best cache report
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <ostream>
 
 int main( int argc, char **argv ) {
   // TODO: implement
-
+  std::cout << "hello world, this is the start to Cache Simulator assignment" << std::endl;
   return 0;
 }
